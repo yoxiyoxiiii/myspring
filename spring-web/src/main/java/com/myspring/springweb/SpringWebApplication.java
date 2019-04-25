860becded75d3cef4ec01e350c1bfed32893fd58
846814595@qq.com <846814595@qq.com>
2019-04-25 23:43:30 +0800

    spring ioc 容器时序图

--- spring-web/src/main/java/com/myspring/springweb/SpringWebApplication.java
+++ spring-web/src/main/java/com/myspring/springweb/SpringWebApplication.java
@@ -1,9 +1,13 @@
 package com.myspring.springweb;
 
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class SpringWebApplication {
 
 	public static void main(String[] args) {
+		ApplicationContext webApplicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 	}
 
 }
